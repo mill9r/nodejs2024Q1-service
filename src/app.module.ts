4e713048f0366e44a2ac3f86fa0a31b8065dfe92
andy_t <andrei.tsylko@rk-software.net>
2024-03-17 22:03:36 +0100

    feat: add typeORM and update user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { DbModule } from './db/db.module';
 import { AlbumsModule } from './albums/albums.module';
 import { TracksModule } from './tracks/tracks.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pwd123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     UsersModule,
     ArtistsModule,
     DbModule,
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,24 @@
-export interface User {
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   login: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
   version: number;
-  createdAt: number;
-  updatedAt: number;
+
+  @Column('bigint')
+  createdAt: string;
+
+  @Column('bigint')
+  updatedAt: string;
 }
